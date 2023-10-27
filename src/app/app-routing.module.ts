188df166d3ff2adb5ecf7f4117de6c81ab7e07c7
Humberto Tejada <humtej1204@gmail.com>
2023-10-27 11:48:10 -0500

    [Feature]: Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/pages/authentication/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'auth'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./views/pages/authentication/authentication.module')
+      .then(m => m.AuthenticationModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./views/pages/dashboard/dashboard.module')
+      .then(m => m.DashboardModule)
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './views/pages/authentication/login/login.component';
+import { RegisterComponent } from './views/pages/authentication/register/register.component';
+import { DashboardComponent } from './views/pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
