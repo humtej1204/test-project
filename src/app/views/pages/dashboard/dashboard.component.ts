b1e844e3d3120133d580cbd3a709582ba3d38bfd
Humberto Tejada <humtej1204@gmail.com>
2023-10-27 21:51:59 -0500

    [Feature]: Get Pokemon Data in Table

--- src/app/views/pages/dashboard/dashboard.component.ts
+++ src/app/views/pages/dashboard/dashboard.component.ts
@@ -12,28 +12,22 @@ import { PokeApiService } from 'src/app/services/pokeApi/poke-api.service';
 })
 
 export class DashboardComponent {
-  ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
   user: User | null = null;
+
+  pokemons: any = [];
 
   constructor(
     private authService: AuthService,
     private pokeServ: PokeApiService,
     private router: Router,
-  ) {}
+  ) {
+    this.authService.myProfile$.subscribe((data: User|null) => {
+      this.user = data;
+    });
+  }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  displayedColumns: string[] = ['id', 'name', 'types', 'img'];
+  dataSource = new MatTableDataSource(this.pokemons);
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -41,11 +35,13 @@ export class DashboardComponent {
   }
 
   ngOnInit(): void {
-    this.authService.myProfile$.subscribe((data: User|null) => {
-      this.user = data;
-    });
+    this.getPokemons();
+  }
 
-    this.pokeServ.getAllPokemons().subscribe(console.log)
+  getPokemons() {
+    this.pokeServ.getAllPokemons().subscribe(res => {
+      this.dataSource = new MatTableDataSource(res);
+    })
   }
 
   logout() {
@@ -53,11 +49,4 @@ export class DashboardComponent {
     this.user = null;
     this.router.navigateByUrl('/auth/login')
   }
-}
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
 }
